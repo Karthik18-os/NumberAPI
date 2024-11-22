acc1f0a7cf445784fb051ac6216291b74d4d6be5
karthik18-os <kartikclt04@gmail.com>
2024-11-22 23:26:29 +0530

    First message

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+import requests 
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    fact = ''
+    if request.method == 'POST':
+        number = request.form['number']
+        response = requests.get(f'http://numbersapi.com/{number}')
+        if response.status_code == 200:
+            fact = response.text
+        else:
+            fact = "Sorry, I couldn't retrieve the fact."
+
+    return render_template('index.html', fact=fact)
+
+if __name__ == '__main__':
+    app.run(debug=True)
